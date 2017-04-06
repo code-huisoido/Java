c71107cd2c1ec2a90e3c1782a166bc606cc842d6
huisoido <584331125@qq.com>
2017-04-06 16:22:55 +0800

    an exercise of the Chapter Four

---
+++ Chapter4/Exercise4.java
@@ -0,0 +1,34 @@
+import static net.mindview.util.Print.*;
+import java.lang.Math.*;
+/*
+ * find prime
+ */
+public class Exercise4 {
+	public static void main(String[] args) {
+		int max = 100;
+		if (args.length != 0) {
+			max = Integer.parseInt(args[0]);	
+		}
+		boolean[] allPrime = new boolean[(max + 1)]; 
+		if (max >= 1) {
+			printnb("prime: " + 1);
+		}
+		if (max >= 2) {
+			printnb(" " + 2);
+		}
+		if (max >= 4) {
+			int limit = (int)Math.floor(Math.sqrt(max));
+			for (int i = 3; i < limit; i += 2) {
+				for (int j = i; i * j <= max; j++) {
+					allPrime[(i*j)] = true;
+				}
+			}
+			for (int i = 3; i <= max; i += 2) {
+				if (!allPrime[i]) {
+					printnb(" " + i);
+				}
+			}
+		}
+		print(" ");
+	}
+}

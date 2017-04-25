6061a1fe83edc30edb021f393ae5de20aca8261e
huisoido <584331125@qq.com>
2017-04-25 17:10:14 +0800

    some demos of the Chapter5

---
+++ chapter5/InitialValues.java
@@ -0,0 +1,29 @@
+import static net.mindview.util.Print.*;
+
+public class InitialValues {
+    boolean t;
+    char c;
+    byte b;
+    short s;
+    int i;
+    long l;
+    float f;
+    double d;
+    InitialValues reference;
+    void printInitialValues() {
+        print("Data type          Initial value");
+        print("boolean            " + t);
+        print("char               " + c);
+        print("byte               " + b);
+        print("short              " + s);
+        print("int                " + i);
+        print("long               " + l);
+        print("float              " + f);
+        print("double             " + d);
+        print("InitialValues      " + reference);
+    }
+
+    public static void main(String[] args) {
+        new InitialValues().printInitialValues();
+    }
+}

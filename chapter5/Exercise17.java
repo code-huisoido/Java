19947c7f8191dc56de151bb7a222e114350ac3c5
huisoido <584331125@qq.com>
2017-04-27 15:35:00 +0800

    some demos of the book

---
+++ chapter5/Exercise17.java
@@ -0,0 +1,9 @@
+public class Exercise17 {
+    Exercise17(String s) {
+        System.out.println("constructor s=" + s);
+    }
+
+    public static void main(String[] args) {
+        Exercise17[] test = new Exercise17[5];
+    }
+}

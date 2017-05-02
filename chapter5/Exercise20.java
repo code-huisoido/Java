7f8e676ad640b87ac08ed3cf71b7215d87db88a4
huisoido <584331125@qq.com>
2017-05-02 17:06:53 +0800

    an exercise of the book

---
+++ chapter5/Exercise20.java
@@ -0,0 +1,8 @@
+public class Exercise20 {
+    public static void main(String... args) {
+        for (String s : args) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+    }
+}

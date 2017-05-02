2421c9753e1caa2c0e22c87b80a72f19781a118a
huisoido <584331125@qq.com>
2017-05-02 11:27:56 +0800

    some demos of the book

---
+++ chapter5/Exercise19.java
@@ -0,0 +1,12 @@
+public class Exercise19 {
+    static void f(String... args) {
+        for (String s : args) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Exercise19.f("hello", "world");
+    }
+}

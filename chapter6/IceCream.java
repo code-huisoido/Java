55d3dc47530f330278f4cb55ea4be36c483bb2e9
huisoido <584331125@qq.com>
2017-05-05 14:21:34 +0800

    some demos of the book

---
+++ chapter6/IceCream.java
@@ -0,0 +1,13 @@
+class Sundae {
+    private Sundae() {}
+    static Sundae makeASundae() {
+        return new Sundae();
+    }
+}
+
+public class IceCream {
+    public static void main(String[] args) {
+        //! Sundae x = new Sundae();
+        Sundae x2 = Sundae.makeASundae();
+    }
+}
--- chapter6/dessert/Cookie.java
+++ chapter6/dessert/Cookie.java
@@ -4,5 +4,7 @@ public class Cookie {
     public Cookie() {
         System.out.println("Cookie constructor");
     }
-    void bite() { System.out.println("bite"); }
+    protected void bite() { 
+        System.out.println("bite"); 
+    }
 }

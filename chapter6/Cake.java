9dc01701b576bc2a561dad05ad1455a31cbc35d0
huisoido <584331125@qq.com>
2017-05-05 11:36:08 +0800

    some demos of the book

---
+++ chapter6/Cake.java
@@ -0,0 +1,6 @@
+class Cake {
+    public static void main(String[] args) {
+        Pie x = new Pie();
+        x.f();
+    }
+}
---
+++ chapter6/Dinner.java
@@ -0,0 +1,8 @@
+import chapter6.dessert.*;
+
+public class Dinner {
+    public static void main(String[] args) {
+        Cookie x = new Cookie();
+        // x.bite();
+    }
+}

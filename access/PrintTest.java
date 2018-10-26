26c0bef54294ef97992dea5c9b23c82938d27d69
huisoido <584331125@qq.com>
2018-10-26 22:36:44 +0800

    some java files

---
+++ access/PrintTest.java
@@ -0,0 +1,10 @@
+import static net.mindview.util.Print.*;
+
+public class PrintTest {
+    public static void main(String[] args) {
+        print("Available from now on!");
+        print(100);
+        print(100L);
+        print(3.14159);
+    }
+}

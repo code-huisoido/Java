e03866c1f88cb9f901bea4e007c819599f7cda07
huisoido <584331125@qq.com>
2019-01-16 21:35:46 +0800

    修改tab为四个空格

--- head_first_java/PhraseOMatic.java
+++ head_first_java/PhraseOMatic.java
@@ -1,21 +1,21 @@
 public class PhraseOMatic {
-	public static void main (String[] args) {
-		String[] wordListOne = {"24/7", "multiTier", "30,000 foot"};
-		String[] wordListTwo = {"empowered", "sticky", "value-added", "oriented", "centric"};
-		String[] wordListThree = {"process", "tiping", "point"};
+    public static void main(String[] args) {
+        String[] wordListOne = {"24/7", "multiTier", "30,000 foot"};
+        String[] wordListTwo = {"empowered", "sticky", "value-added", "oriented", "centric"};
+        String[] wordListThree = {"process", "tiping", "point"};
 
-		int oneLength = wordListOne.length;
-		int twoLength = wordListTwo.length;
-		int threeLength = wordListThree.length;
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
 
-		int rand1 = (int) (Math.random() * oneLength);
-		int rand2 = (int) (Math.random() * twoLength);
-		int rand3 = (int) (Math.random() * threeLength);
-		double random = Math.random();
+        int rand1 = (int) (Math.random() * oneLength);
+        int rand2 = (int) (Math.random() * twoLength);
+        int rand3 = (int) (Math.random() * threeLength);
+        double random = Math.random();
 
-		String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
+        String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
 
-		System.out.println("What we need is a " + phrase);
-		System.out.println(random);
-	}
+        System.out.println("What we need is a " + phrase);
+        System.out.println(random);
+    }
 }
--- head_first_java/Player.java
+++ head_first_java/Player.java
@@ -1,8 +1,8 @@
 public class Player {
-	int number = 0;
+    int number = 0;
 
-	public void guess() {
-		number = (int) (Math.random() * 10);
-		System.out.println("I'm guessing " + number);
-	}
+    public void guess() {
+        number = (int) (Math.random() * 10);
+        System.out.println("I'm guessing " + number);
+    }
 }
